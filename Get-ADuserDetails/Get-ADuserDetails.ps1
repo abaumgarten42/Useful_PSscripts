479dd99dd8200c6d8ca34f897ae8e939104aec72
Andreas Baumgarten <71793682+abaumgarten42@users.noreply.github.com>
2021-04-30 22:18:52 +0200

    Initial

---
+++ Get-ADuserDetails/Get-ADuserDetails.ps1
@@ -0,0 +1,31 @@
+# Get details and groupmemberships of AD user
+
+Import-Module ActiveDirectory
+# List of users
+$users = "mmouse","ppan1234"
+
+$Table = @() # clear table variable
+foreach ($user in $users) {
+    $grp = "" # clear grp variable
+    $Row = [ordered]@{ # build hashtable
+        SamAccountName = ""
+        DisplayName    = ""
+        Email          = ""
+        Groups         = ""
+    }
+    $a = Get-AdUser -Identity $user -Properties * # get user
+    $groups = $a.MemberOf # groupmemberships of user
+    foreach ($g in $groups) {
+        $b = Get-ADGroup $g # get groups
+        $grp += $b.Name + "," # prepate group output
+    }
+    $grp = $grp.Substring(0, $grp.Length - 1) 
+    # build row data
+    $row.SamAccountName = $a.SamAccountName
+    $row.DisplayName = $a.DisplayName
+    $row.Email = $a.EmailAddress
+    $row.Groups = $grp
+    $objRow = New-Object PSObject -Property $row # prepare row object
+    $Table += $objRow # add row to table
+}
+$Table
