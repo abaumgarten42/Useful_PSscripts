634196ea6c5da831f863ed3a03aca290be041c22
Andreas Baumgarten <abaumgarten@outlook.com>
2021-05-08 01:15:04 +0200

    Update

--- variables-ScriptAndFunctions/variables-ScriptAndFunctions.ps1
+++ variables-ScriptAndFunctions/variables-ScriptAndFunctions.ps1
@@ -5,7 +5,7 @@ $A = "Variable A set in script - parent scope"
 $B = "Variable B set in script - parent scope"
 function TestFunction {
     # Set variable in function (A = script scope / parent)
-    $script:A = "Variable A set in function"
+    $script:A = "Variable A set in function with script/parent scope"
     # Set variable in function (B = function scope / child)
     $B = "Variable B set in function - child scope"
 }
