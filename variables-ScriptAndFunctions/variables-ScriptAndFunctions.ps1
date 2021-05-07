b4a54f5899fe9ed2f17219b1c0d959d3e576ebe9
Andreas Baumgarten <abaumgarten@outlook.com>
2021-05-08 01:09:08 +0200

    Initial

---
+++ variables-ScriptAndFunctions/variables-ScriptAndFunctions.ps1
@@ -0,0 +1,16 @@
+# Working with variables in function / parent + child scope of functions
+
+# Set variables in script (parent scope)
+$A = "Variable A set in script - parent scope"
+$B = "Variable B set in script - parent scope"
+function TestFunction {
+    # Set variable in function (A = script scope / parent)
+    $script:A = "Variable A set in function"
+    # Set variable in function (B = function scope / child)
+    $B = "Variable B set in function - child scope"
+}
+# Call function
+TestFunction
+# Result
+$A
+$B
