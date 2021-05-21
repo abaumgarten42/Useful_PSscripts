55c953e8e75e427c7ff5537b84daebd3384ca1f7
Andreas Baumgarten <abaumgarten@outlook.com>
2021-05-22 00:52:34 +0200

    Update

--- Add-QuotesAroundVariableValue/Add-QuotesAroundVariableValue.ps1
+++ Add-QuotesAroundVariableValue/Add-QuotesAroundVariableValue.ps1
@@ -3,3 +3,10 @@
 $outputExample = 'C:\Program Files\WinRAR\uninstall.exe param1,param2'
 $outputExampleWithQuotes = '"{0}"' -f $outputExample
 $outputExampleWithQuotes
+
+# Add double quotes around a variable value / without params
+$outputExample = 'C:\Program Files\WinRAR\uninstall.exe param1,param2'
+$params = ((Split-Path -Path $outputExample -Leaf).Split(" "))[1]
+$outputFileAndPath = ($outputExample.replace($params,"")).Trim()
+$outputExampleWithQuotes = ('"{0}"' -f $outputFileAndPath) + " $params"
+$outputExampleWithQuotes
