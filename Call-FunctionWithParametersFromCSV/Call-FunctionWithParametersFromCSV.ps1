caeac082440e9202aba5907f926dc4f946fdd9a4
[Andreas Baumgarten] <abaumgarten@outlook.com>
2021-07-11 10:21:03 +0200

    Initial Upload

---
+++ Call-FunctionWithParametersFromCSV/Call-FunctionWithParametersFromCSV.ps1
@@ -0,0 +1,18 @@
+## Call a Function and read parameters from CSV file
+
+# Define Function 
+function JustForTest {
+    param (
+        [string]$wlidsvc,
+        [string]$Action,
+        [string]$ServiceStatus
+    )
+    # Put your code here
+    Write-Output "Service = $wlidsvc - Action = $Action - Status  = $ServiceStatus"
+}
+# Read CSV file with parameters
+$lines = Import-Csv -Path test1.csv -Delimiter ","
+# Execute function for each line in CSV using the parameter values
+foreach ($line in $lines) {
+    JustForTest -wlidsvc $line.wlidsvc -Action $line.Action -ServiceStatus $line.ServiceStatus
+}
