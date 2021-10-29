7f29c8df55fde91b70c678ea1c70a063051feef7
Andreas Baumgarten <abaumgarten@outlook.com>
2021-10-29 23:47:08 +0200

    Update

---
+++ Get-AzureDiskAndRelatedVMs/Get-AzureDiskAndRelatedVMs.ps1
@@ -0,0 +1,18 @@
+# Get all disks in Azure in a Subcription + related VM
+# Requires the AZ cmdlets
+# Login with Connect-AzAccount
+# Set conbtext with Set-AzContext -Name "<Name of Subscription>"
+# or Connect-AzAccount -SubscriptionID "<Subscription ID>"
+
+$result = "DiskName,DiskResourceGroupName,VMname,VMResourceGroupName `r`n"
+$diskObjects = get-azdisk
+foreach ($diskObj in $diskObjects) {
+    $diskName = $diskObj.Name    
+    $diskRG = $diskObj.ResourceGroupName
+    $relVMname = ($diskObj.ManagedBy).Split("/")[-1]
+    $VMobj = Get-AZVM -Name $relVMname
+    $vmName = $VMobj.Name
+    $vmRG = $VMobj.ResourceGroupName
+    $result += "$diskName" +","+ "$diskRG" +","+ "$vmName" +","+ "$vmRG" + "`r`n"
+    }
+$result
