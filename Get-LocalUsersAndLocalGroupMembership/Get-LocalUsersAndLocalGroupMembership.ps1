fe01a277b271b57657d46929344fbb37062cc0c9
[Andreas Baumgarten] <abaumgarten@outlook.com>
2022-09-15 14:04:23 +0200

    Update

--- Get-LocalUsersAndLocalGroupMembership/Get-LocalUsersAndLocalGroupMembership.ps1
+++ Get-LocalUsersAndLocalGroupMembership/Get-LocalUsersAndLocalGroupMembership.ps1
@@ -1,3 +1,4 @@
+# List local users and localgroup memberships
 $ADSI = [ADSI]"WinNT://$env:COMPUTERNAME"
 $ADSI.Children | Where-Object { $_.SchemaClassName -eq "User" } | ForEach-Object {
     $GROUPS = $_.Groups() | ForEach-Object { $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null) }
